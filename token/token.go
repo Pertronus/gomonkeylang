e5ba940d169a674a6480813715347db79bdae763
David Hayes <dmhayes82@gmail.com>
2025-04-12 21:12:25 +0100

    start of monkey lexer

---
+++ token/token.go
@@ -0,0 +1,37 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+	IDENT     = "IDENT" // Identifiers and literals
+	INT       = "INT"
+	ASSIGN    = "=" // Operators
+	PLUS      = "+"
+	COMMA     = "," // Delimiters
+	SEMICOLON = ";"
+	LPARAN    = "(" // Braces
+	RPARAN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	FUNCTION  = "FUNCTION" // Keywords
+	LET       = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIndent(indent string) TokenType {
+	if tok, ok := keywords[indent]; ok {
+		return tok
+	}
+	return IDENT
+}
